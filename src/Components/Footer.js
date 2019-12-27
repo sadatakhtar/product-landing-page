c9eee658a868b6d840e241e9f764d1e4aa112b97
sadat akhtar <sadat0akhtar@gmail.com>
2019-12-27 17:33:46 +0000

    created Components folder with header and footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Footer(){
+    return (
+        <h2>footer</h2>
+    )
+}
+export default Footer;
---
+++ src/Components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Header(){
+    return (
+        <header>
+            <h1>Haberdashery</h1>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Products</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+export default Header;
