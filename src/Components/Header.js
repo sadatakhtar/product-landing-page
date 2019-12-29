83f2179e85980e7ce5b8417c3c1aed27e5b2d243
sadat akhtar <sadat0akhtar@gmail.com>
2019-12-29 16:07:15 +0000

    added video link to nav-link

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,12 +6,12 @@ function Header(){
         <header id="header">
             
             <nav id="nav-bar">
-            <img src={yarn} alt="yarn" width="70px" height="60px" />
+            <img id="header-img" src={yarn} alt="yarn" width="70px" height="60px" />
                 <h1 className="title">Haberdashery</h1>
                 <ul>
                     <li className="nav-link"><a href="#">Home</a></li>
                     <li><a href="#">Products</a></li>
-                    <li><a href="#">Pricing</a></li>
+                    <li><a href="#video">Video</a></li>
                     <li><a href="#">Contact</a></li>
                 </ul>
             </nav>
